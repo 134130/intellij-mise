0a13b9d53ead081ce62d1f9180e87fba6dcc1fc6
Cooper Oh <cooper@querypie.com>
2024-11-09 14:31:04 +0900

    support offline flag

---
+++ modules/core/src/main/kotlin/com/github/l34130/mise/commands/MiseVersion.kt
@@ -0,0 +1,42 @@
+package com.github.l34130.mise.commands
+
+data class MiseVersion(
+    val major: Int,
+    val minor: Int,
+    val patch: Int,
+) : Comparable<MiseVersion> {
+    companion object {
+        fun from(str: String): MiseVersion {
+            val versionStr = str.split(" ").firstOrNull()
+
+            requireNotNull(versionStr) { "Invalid version string: $str" }
+
+            val split = versionStr.split(".")
+            require(split.size == 3) { "Invalid version string: $str" }
+
+            return MiseVersion(split[0].toInt(), split[1].toInt(), split[2].toInt())
+        }
+    }
+
+    override fun compareTo(other: MiseVersion): Int {
+        if (major > other.major) {
+            return 1
+        } else if (major < other.major) {
+            return -1
+        }
+
+        if (minor > other.minor) {
+            return 1
+        } else if (minor < other.minor) {
+            return -1
+        }
+
+        if (patch > other.patch) {
+            return 1
+        } else if (patch < other.patch) {
+            return -1
+        }
+
+        return 0
+    }
+}
