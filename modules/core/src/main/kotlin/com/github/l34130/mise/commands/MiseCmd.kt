aa5eb731f869a4dc165d5dac82cc15c6f29a1b24
오지환 (Cooper) <hwan_@kakao.com>
2024-11-09 15:47:30 +0900

    support offline flag (#51)

--- modules/core/src/main/kotlin/com/github/l34130/mise/commands/MiseCmd.kt
+++ modules/core/src/main/kotlin/com/github/l34130/mise/commands/MiseCmd.kt
@@ -37,25 +37,58 @@ object MiseCmd {
 
     fun loadTools(workDir: String?): Map<String, List<MiseTool>> {
         try {
-            val process =
-                GeneralCommandLine("mise", "ls", "--current", "--json")
-                    .withWorkDirectory(workDir)
-                    .createProcess()
+            val miseVersionStr =
+                runCommandLine(listOf("mise", "version"), workDir).getOrNull()
 
-            val exitCode = process.waitFor()
-            if (exitCode != 0) {
-                val stderr = process.errorReader().use { it.readText() }
-                Notification.notify("Failed to import Mise tools: $stderr", NotificationType.WARNING)
+            val miseVersion =
+                if (miseVersionStr == null) {
+                    MiseVersion(0, 0, 0)
+                } else {
+                    MiseVersion.from(miseVersionStr)
+                }
+
+            // https://github.com/jdx/mise/commit/6e7e4074989bda47e40900cb651b694c72d39f4d
+            val supportsOfflineFlag = miseVersion >= MiseVersion(2024, 11, 4)
+            val cliResult =
+                if (supportsOfflineFlag) {
+                    runCommandLine(listOf("mise", "ls", "--current", "--json", "--offline"), workDir)
+                } else {
+                    runCommandLine(listOf("mise", "ls", "--current", "--json"), workDir)
+                }
+
+            if (cliResult.isFailure) {
+                Notification.notify(
+                    "Failed to import Mise tools: ${cliResult.exceptionOrNull() ?: "Unknown exception"}",
+                    NotificationType.WARNING,
+                )
                 return emptyMap()
             }
 
             return GsonBuilder()
                 .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
                 .create()
-                .fromJson<Map<String, List<MiseTool>>>(process.inputReader())
+                .fromJson<Map<String, List<MiseTool>>>(cliResult.getOrThrow())
         } catch (e: Exception) {
             Notification.notify("Failed to import Mise tools: ${e.message}", NotificationType.ERROR)
             return emptyMap()
         }
+    }
+
+    private fun runCommandLine(
+        commandLineArgs: List<String>,
+        workDir: String?,
+    ): Result<String> {
+        val process =
+            GeneralCommandLine(commandLineArgs)
+                .withWorkDirectory(workDir)
+                .createProcess()
+
+        val exitCode = process.waitFor()
+        if (exitCode != 0) {
+            val stderr = process.errorReader().use { it.readText() }
+            throw RuntimeException(stderr)
+        }
+
+        return Result.success(process.inputReader().use { it.readText() })
     }
 }
