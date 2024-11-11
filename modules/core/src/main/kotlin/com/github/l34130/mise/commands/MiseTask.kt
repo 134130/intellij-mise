459d4c42a23d48122c123f26734b46f633714e3f
hverlin <hugues.verlin@alteryx.com>
2024-11-11 02:24:10 +0100

    Add mise panel (display tasks, env, tool) (#69)
    Resolves #65 
    
    <img width="1512" alt="image"
    src="https://github.com/user-attachments/assets/a58564e3-6e09-4816-8e55-6dd2b82b39f6">
    
    Display tasks, tool and env variables.
    I think there is no way to group env. variables by mise files now. I
    might open an issue on the mise repository
    
    ---------
    
    Co-authored-by: Cooper Oh <cooper@querypie.com>

--- modules/core/src/main/kotlin/com/github/l34130/mise/commands/MiseTask.kt
+++ modules/core/src/main/kotlin/com/github/l34130/mise/commands/MiseTask.kt
@@ -6,6 +6,8 @@ data class MiseTask(
     val depends: List<String>? = null,
     val description: String? = null,
     val hide: Boolean = false,
+    val source: String? = null,
 ) {
-    override fun toString(): String = "MiseTask(name='$name', aliases=$aliases, depends=$depends, description=$description, hide=$hide)"
+    override fun toString(): String =
+        "MiseTask(name='$name', aliases=$aliases, depends=$depends, description=$description, hide=$hide, source=$source)"
 }
---
+++ modules/core/src/main/kotlin/com/github/l34130/mise/toolWindow/MisePanelWindowFactory.kt
@@ -0,0 +1,22 @@
+package com.github.l34130.mise.toolWindow
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.ui.content.ContentFactory
+
+class MisePanelWindowFactory : ToolWindowFactory {
+    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        val misePanel = MisePanel(project)
+
+        toolWindow.setTitleActions(
+            listOf(
+                SettingsAction(project),
+                RefreshAction(misePanel)
+            )
+        )
+
+        val content = ContentFactory.getInstance().createContent(misePanel, "", false)
+        toolWindow.contentManager.addContent(content)
+    }
+}
