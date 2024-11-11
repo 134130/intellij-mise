1f331fbc3c0a0a2a6f47fe0ff2d8cf703782e929
Cooper Oh <cooper@querypie.com>
2024-11-11 10:30:17 +0900

    Make MiseSettings as project scoped

--- modules/core/src/main/kotlin/com/github/l34130/mise/settings/MiseSettings.kt
+++ modules/core/src/main/kotlin/com/github/l34130/mise/settings/MiseSettings.kt
@@ -2,31 +2,41 @@ package com.github.l34130.mise.settings
 
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.State
 import com.intellij.openapi.components.Storage
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
 import com.intellij.util.messages.Topic
 
 data class MiseState(
     var isMiseEnabled: Boolean = true,
-    var miseProfile: String = "",
-)
+    var miseProfile: String = ".",
+) : Cloneable {
+    public override fun clone(): MiseState =
+        MiseState(
+            isMiseEnabled = isMiseEnabled,
+            miseProfile = miseProfile,
+        )
+}
 
+@Service(Service.Level.PROJECT)
 @State(name = "com.github.l34130.mise.settings.MiseSettings", storages = [Storage("mise.xml")])
-class MiseSettings private constructor() : PersistentStateComponent<MiseState> {
-    override fun getState() = STATE
+class MiseSettings(
+    private val project: Project,
+) : PersistentStateComponent<MiseState> {
+    private var state = MiseState()
+
+    override fun getState() = state
 
     override fun loadState(state: MiseState) {
-        val oldState = STATE.copy()
-        STATE.isMiseEnabled = state.isMiseEnabled
-        STATE.miseProfile = state.miseProfile
-        ApplicationManager.getApplication().messageBus
-            .syncPublisher(MISE_SETTINGS_TOPIC)
-            .settingsChanged(oldState, state)
+        this.state = state.clone()
     }
 
     companion object {
+        fun getService(project: Project): MiseSettings = project.service<MiseSettings>()
+
         val MISE_SETTINGS_TOPIC = Topic.create("Mise Settings", SettingsChangeListener::class.java)
-        private val STATE = MiseState()
         val instance: MiseSettings = ApplicationManager.getApplication().getService(MiseSettings::class.java)
     }
 
