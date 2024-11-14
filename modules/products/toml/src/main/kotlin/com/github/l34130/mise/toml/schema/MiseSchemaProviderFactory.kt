8ac435e0c86c74b7d652b63ec04c5689c72510be
Cooper Oh <cooper@chequer.io>
2024-11-15 01:03:47 +0900

    drop schema provider

--- modules/products/toml/src/main/kotlin/com/github/l34130/mise/toml/schema/MiseSchemaProviderFactory.kt
+++
@@ -1,9 +0,0 @@
-package com.github.l34130.mise.toml.schema
-
-import com.intellij.openapi.project.Project
-import com.jetbrains.jsonSchema.extension.JsonSchemaFileProvider
-import com.jetbrains.jsonSchema.extension.JsonSchemaProviderFactory
-
-class MiseSchemaProviderFactory : JsonSchemaProviderFactory {
-    override fun getProviders(project: Project): List<JsonSchemaFileProvider> = listOf(MiseSchemaFileProvider(project))
-}
