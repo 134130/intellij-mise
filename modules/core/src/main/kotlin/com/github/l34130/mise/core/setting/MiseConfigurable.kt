38024f318fba78e2141e6a353c51e24cbfde6f28
오지환 (Cooper) <hwan_@kakao.com>
2024-11-23 18:34:41 +0900

    feat: Support Mise executable customization (#95)
    ## Description
    - Closes #70 
    - Support custom mise executable path. It auto detects from PATH
    default.

--- modules/core/src/main/kotlin/com/github/l34130/mise/core/setting/MiseConfigurable.kt
+++ modules/core/src/main/kotlin/com/github/l34130/mise/core/setting/MiseConfigurable.kt
@@ -1,12 +1,15 @@
 package com.github.l34130.mise.core.setting
 
+import com.intellij.openapi.fileChooser.FileChooserDescriptor
 import com.intellij.openapi.options.SearchableConfigurable
 import com.intellij.openapi.project.Project
 import com.intellij.ui.components.JBCheckBox
 import com.intellij.ui.components.JBTextField
+import com.intellij.ui.components.textFieldWithHistoryWithBrowseButton
 import com.intellij.ui.dsl.builder.COLUMNS_LARGE
 import com.intellij.ui.dsl.builder.columns
 import com.intellij.ui.dsl.builder.panel
+import com.intellij.ui.layout.selected
 import java.awt.BorderLayout
 import javax.swing.JComponent
 import javax.swing.JPanel
@@ -25,38 +28,45 @@ class MiseConfigurable(
         myMiseDirEnvCb.isSelected = service.state.useMiseDirEnv
         myMiseProfileTf.text = service.state.miseProfile
 
-        if (!service.state.useMiseDirEnv) {
-            myMiseProfileTf.isEnabled = false
-        }
-
-        myMiseDirEnvCb.addChangeListener {
-            myMiseProfileTf.isEnabled = myMiseDirEnvCb.isSelected
-        }
-
         return JPanel(BorderLayout()).apply {
             add(
                 panel {
-                    row {
-                        label("Environments").bold()
+                    row("Mise Executable:") {
+                        cell(
+                            textFieldWithHistoryWithBrowseButton(
+                                project = project,
+                                browseDialogTitle = "Select Mise Executable",
+                                fileChooserDescriptor = FileChooserDescriptor(true, false, false, false, false, false),
+                                historyProvider = { listOf("/opt/homebrew/bin/mise").distinct() },
+                            ).apply {
+                                setTextAndAddToHistory(service.state.executablePath)
+                                setTextFieldPreferredWidth(50)
+                            }
+                        ).comment(
+                            """
+                            Specify the path to the mise executable.</br>
+                            Not installed? Visit the <a href='https://mise.jdx.dev/installing-mise.html'>mise installation</a>
+                            """.trimIndent()
+                        ).resizableColumn()
                     }
 
-                    row {
-                        panel {
-                            row {
-                                cell(myMiseDirEnvCb).comment(
-                                    "Load environment variables from mise configuration file(s)",
-                                )
-                            }
-                            row("Profile: ") {
-                                cell(myMiseProfileTf)
-                                    .comment(
-                                        "Specify the mise profile to use (leave empty for default)" +
-                                            "<br/><a href='https://mise.jdx.dev/profiles.html#profiles'>Learn more about mise profiles</a>",
-                                    ).columns(COLUMNS_LARGE)
-                                    .focused()
-                                    .resizableColumn()
-                            }
+                    group("Environments") {
+                        row {
+                            cell(myMiseDirEnvCb).comment(
+                                "Load environment variables from mise configuration file(s)",
+                            )
                         }
+                        row("Profile:") {
+                            cell(myMiseProfileTf)
+                                .comment(
+                                    """
+                                    Specify the mise profile to use (leave empty for default)<br/>
+                                    <a href='https://mise.jdx.dev/profiles.html#profiles'>Learn more about mise profiles</a>
+                                    """.trimIndent(),
+                                ).columns(COLUMNS_LARGE)
+                                .focused()
+                                .resizableColumn()
+                        }.enabledIf(myMiseDirEnvCb.selected)
                     }
                 },
             )
--- modules/core/src/main/kotlin/com/github/l34130/mise/core/setting/MiseSettings.kt
+++ modules/core/src/main/kotlin/com/github/l34130/mise/core/setting/MiseSettings.kt
@@ -1,19 +1,27 @@
 package com.github.l34130.mise.core.setting
 
+import com.github.l34130.mise.core.notification.NotificationService
+import com.intellij.notification.NotificationAction
 import com.intellij.openapi.components.PersistentStateComponent
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.State
 import com.intellij.openapi.components.Storage
 import com.intellij.openapi.components.service
+import com.intellij.openapi.options.ShowSettingsUtil
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.util.EnvironmentUtil
 import com.intellij.util.messages.Topic
+import java.io.File
 
 data class MiseState(
+    var executablePath: String = "",
     var useMiseDirEnv: Boolean = true,
     var miseProfile: String = "",
 ) : Cloneable {
     public override fun clone(): MiseState =
         MiseState(
+            executablePath = executablePath,
             useMiseDirEnv = useMiseDirEnv,
             miseProfile = miseProfile,
         )
@@ -30,10 +38,54 @@ class MiseSettings : PersistentStateComponent<MiseState> {
         this.state = state.clone()
     }
 
+    override fun noStateLoaded() {
+        state = MiseState(
+            executablePath = getMiseExecutablePath() ?: "",
+        )
+    }
+
+    override fun initializeComponent() {
+        state = MiseState(
+            executablePath = state.executablePath.takeIf { it.isNotEmpty() } ?: getMiseExecutablePath() ?: "",
+            useMiseDirEnv = state.useMiseDirEnv,
+            miseProfile = state.miseProfile,
+        )
+
+        if (state.executablePath.isEmpty()) {
+            NotificationService.getInstance().warn(
+                title = "Mise Executable Not Found",
+                htmlText = """
+                    Mise executable not found in PATH.<br/>
+                    Please specify the path to the mise executable in the settings.
+                """.trimIndent(),
+                actionProvider = {
+                    NotificationAction.createSimple(
+                        "Open settings",
+                    ) {
+                        ShowSettingsUtil.getInstance().showSettingsDialog(null, MiseConfigurable::class.java)
+                    }
+                },
+            )
+        }
+    }
+
     companion object {
         fun getService(project: Project): MiseSettings = project.service<MiseSettings>()
 
         val MISE_SETTINGS_TOPIC = Topic.create("Mise Settings", SettingsChangeListener::class.java)
+
+        private fun getMiseExecutablePath(): String? {
+            val path = EnvironmentUtil.getValue("PATH") ?: return null
+
+            for (dir in StringUtil.tokenize(path, File.pathSeparator)) {
+                val file = File(dir, "mise")
+                if (file.canExecute()) {
+                    return file.absolutePath
+                }
+            }
+
+            return null
+        }
     }
 
     interface SettingsChangeListener {
