28d4fd241cd1dd872435f83585b5c141898f8856
JiHwan Oh <hwan_@kakao.com>
2025-01-05 19:57:20 +0900

    Introduce editor

--- modules/core/src/main/kotlin/com/github/l34130/mise/core/execution/configuration/MiseTomlTaskRunConfiguration.kt
+++ modules/core/src/main/kotlin/com/github/l34130/mise/core/execution/configuration/MiseTomlTaskRunConfiguration.kt
@@ -2,6 +2,7 @@ package com.github.l34130.mise.core.execution.configuration
 
 import com.github.l34130.mise.core.setting.MiseSettings
 import com.intellij.execution.Executor
+import com.intellij.execution.configuration.EnvironmentVariablesData
 import com.intellij.execution.configurations.CommandLineState
 import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.execution.configurations.LocatableConfigurationBase
@@ -16,43 +17,76 @@ import com.intellij.openapi.components.service
 import com.intellij.openapi.options.SettingsEditor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.encoding.EncodingManager
+import com.intellij.util.EnvironmentUtil
 import com.intellij.util.application
+import org.jdom.Element
 
 class MiseTomlTaskRunConfiguration(
     project: Project,
     factory: MiseTomlTaskRunConfigurationFactory,
     name: String,
 ) : LocatableConfigurationBase<RunProfileState>(project, factory, name) {
+    private val settings = application.service<MiseSettings>().state
+
+    var miseExecutablePath: String = settings.executablePath
+    var miseConfigEnvironment: String? = settings.miseConfigEnvironment
+    var miseTaskName: String = ""
+    var workingDirectory: String? = project.basePath
+    var envVars: EnvironmentVariablesData = EnvironmentVariablesData.DEFAULT
+
     override fun getState(
         executor: Executor,
         executionEnvironment: ExecutionEnvironment,
     ): RunProfileState {
-        val settings = application.service<MiseSettings>().state
-        val executablePath = settings.executablePath
-
         return object : CommandLineState(executionEnvironment) {
             override fun startProcess(): ProcessHandler {
                 val macroManager = PathMacroManager.getInstance(project)
-                // val localFile = macroManager.expandPath()
+                val workDirectory =
+                    workingDirectory?.let { macroManager.expandPath(it) }
+                        ?: project.basePath
+
+                val params = mutableListOf<String>()
+                miseConfigEnvironment?.let {
+                    params += listOf("--env", it)
+                }
+                params += listOf("run", miseTaskName)
 
                 val commandLine =
                     GeneralCommandLine()
-                        .withExePath(executablePath)
-//                    .withWorkDirectory()
-//                    .withEnvironment()
+                        .withExePath(miseExecutablePath)
+                        .withWorkDirectory(workDirectory)
+                        .withEnvironment(EnvironmentUtil.getEnvironmentMap() + envVars.envs)
                         .withParentEnvironmentType(GeneralCommandLine.ParentEnvironmentType.CONSOLE)
-                        .withParameters(listOf())
+                        .withParameters(params)
                         .withCharset(EncodingManager.getInstance().defaultConsoleEncoding)
 
-                val processHandler = ColoredProcessHandler(commandLine)
-                processHandler.setShouldKillProcessSoftly(true)
-                ProcessTerminatedListener.attach(processHandler)
-                return processHandler
+                return ColoredProcessHandler(commandLine).apply {
+                    setShouldKillProcessSoftly(true)
+                    ProcessTerminatedListener.attach(this)
+                }
             }
         }
     }
 
-    override fun getConfigurationEditor(): SettingsEditor<out RunConfiguration> {
-        TODO("Not yet implemented")
+    override fun getConfigurationEditor(): SettingsEditor<out RunConfiguration> = MiseTomlTaskRunConfigurationEditor(project)
+
+    override fun writeExternal(element: Element) {
+        super.writeExternal(element)
+        val child = element.getOrCreateChild("mise")
+        child.setAttribute("executablePath", miseExecutablePath)
+        child.setAttribute("configEnvironment", miseConfigEnvironment ?: "")
+        child.setAttribute("taskName", miseTaskName)
+        child.setAttribute("workingDirectory", workingDirectory ?: "")
+        envVars.writeExternal(child)
+    }
+
+    override fun readExternal(element: Element) {
+        super.readExternal(element)
+        val child = element.getChild("mise") ?: return
+        miseExecutablePath = child.getAttributeValue("executablePath") ?: ""
+        miseConfigEnvironment = child.getAttributeValue("configEnvironment")
+        miseTaskName = child.getAttributeValue("taskName") ?: ""
+        workingDirectory = child.getAttributeValue("workingDirectory")
+        envVars = EnvironmentVariablesData.readExternal(child)
     }
 }
---
+++ modules/core/src/main/kotlin/com/github/l34130/mise/core/execution/configuration/MiseTomlTaskRunConfigurationEditor.kt
@@ -0,0 +1,80 @@
+package com.github.l34130.mise.core.execution.configuration
+
+import com.github.l34130.mise.core.lang.MiseTomlFileType
+import com.github.l34130.mise.core.setting.MiseSettings
+import com.intellij.execution.configuration.EnvironmentVariablesComponent
+import com.intellij.openapi.components.service
+import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
+import com.intellij.openapi.options.SettingsEditor
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.TextFieldWithBrowseButton
+import com.intellij.ui.components.JBTextField
+import com.intellij.ui.dsl.builder.AlignX
+import com.intellij.ui.dsl.builder.panel
+import com.intellij.util.application
+import com.intellij.util.ui.JBUI
+import javax.swing.JComponent
+
+class MiseTomlTaskRunConfigurationEditor(
+    private val project: Project,
+) : SettingsEditor<MiseTomlTaskRunConfiguration>() {
+    private val applicationState = application.service<MiseSettings>().state
+
+    private val miseExecutablePathTf = TextFieldWithBrowseButton()
+    private val miseConfigEnvironmentTf = JBTextField()
+    private val miseTaskNameTf = JBTextField()
+    private val workingDirectoryTf = TextFieldWithBrowseButton()
+    private val envVarsComponent = EnvironmentVariablesComponent()
+
+    init {
+        miseExecutablePathTf.addBrowseFolderListener(
+            "Mise Executable Path",
+            "Select the Mise executable path",
+            project,
+            FileChooserDescriptorFactory.createSingleFileDescriptor(MiseTomlFileType),
+        )
+        workingDirectoryTf.addBrowseFolderListener(
+            "Working Directory",
+            "Select the working directory",
+            project,
+            FileChooserDescriptorFactory.createSingleFolderDescriptor(),
+        )
+    }
+
+    override fun createEditor(): JComponent =
+        panel {
+            row("Mise executable:") {
+                cell(miseExecutablePathTf).align(AlignX.FILL)
+            }
+            row("Mise config environment:") {
+                cell(miseConfigEnvironmentTf).align(AlignX.FILL)
+            }
+            row("Mise task name:") {
+                cell(miseTaskNameTf).align(AlignX.FILL)
+            }
+            row("Working directory:") {
+                cell(workingDirectoryTf).align(AlignX.FILL)
+            }
+            row("Environment variables:") {
+                cell(envVarsComponent).align(AlignX.FILL)
+            }
+        }.apply {
+            border = JBUI.Borders.empty(6, 16, 16, 16)
+        }
+
+    override fun resetEditorFrom(configuration: MiseTomlTaskRunConfiguration) {
+        miseExecutablePathTf.text = configuration.miseExecutablePath
+        miseConfigEnvironmentTf.text = configuration.miseConfigEnvironment
+        miseTaskNameTf.text = configuration.miseTaskName
+        workingDirectoryTf.text = configuration.workingDirectory ?: ""
+        envVarsComponent.envData = configuration.envVars
+    }
+
+    override fun applyEditorTo(configuration: MiseTomlTaskRunConfiguration) {
+        configuration.miseExecutablePath = miseExecutablePathTf.text
+        configuration.miseConfigEnvironment = miseConfigEnvironmentTf.text
+        configuration.miseTaskName = miseTaskNameTf.text
+        configuration.workingDirectory = workingDirectoryTf.text
+        configuration.envVars = envVarsComponent.envData
+    }
+}
