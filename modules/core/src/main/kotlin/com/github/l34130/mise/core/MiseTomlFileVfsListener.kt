792d559e2f58b1a128e319263302b1e84d94a8f4
JiHwan Oh <hwan_@kakao.com>
2025-01-29 20:01:23 +0900

    comment-out unused codes

--- modules/core/src/main/kotlin/com/github/l34130/mise/core/MiseTomlFileVfsListener.kt
+++ modules/core/src/main/kotlin/com/github/l34130/mise/core/MiseTomlFileVfsListener.kt
@@ -2,8 +2,6 @@ package com.github.l34130.mise.core
 
 import com.github.l34130.mise.core.lang.MiseTomlFileType
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer
-import com.intellij.openapi.editor.EditorFactory
-import com.intellij.openapi.editor.ex.EditorEx
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.ZipperUpdater
 import com.intellij.openapi.vfs.VirtualFile
@@ -14,8 +12,6 @@ import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
 import com.intellij.psi.PsiTreeAnyChangeAbstractAdapter
 import com.intellij.util.Alarm
-import com.intellij.util.application
-import com.intellij.util.concurrency.SequentialTaskExecutor
 import com.intellij.util.messages.MessageBusConnection
 import com.intellij.util.messages.Topic
 import kotlinx.coroutines.Runnable
@@ -59,8 +55,7 @@ class MiseTomlFileVfsListener private constructor(
         val project: Project,
         val service: MiseService,
     ) {
-        private val updater = ZipperUpdater(200, Alarm.ThreadToUse.POOLED_THREAD, application)
-        private val taskExecutor = SequentialTaskExecutor.createSequentialApplicationPoolExecutor("MiseLocalIndexVfsListener")
+        private val updater = ZipperUpdater(200, Alarm.ThreadToUse.POOLED_THREAD, service)
         private val dirtyTomlFiles: MutableSet<VirtualFile> = ConcurrentHashMap.newKeySet<VirtualFile>()
         private val runnable =
             Runnable {
@@ -69,14 +64,14 @@ class MiseTomlFileVfsListener private constructor(
                 project.messageBus.syncPublisher(MISE_TOML_CHANGED).run()
                 dirtyTomlFiles.removeAll(scope)
 
-                val analyzer = DaemonCodeAnalyzer.getInstance(project)
-                val psiManager = PsiManager.getInstance(project)
-                val editors = EditorFactory.getInstance().allEditors
-                for (editor in editors) {
-                    if (editor !is EditorEx || editor.project != project) continue
-
-                    val file = editor.virtualFile
-                    if (file == null || !file.isValid) continue
+//                val analyzer = DaemonCodeAnalyzer.getInstance(project)
+//                val psiManager = PsiManager.getInstance(project)
+//                val editors = EditorFactory.getInstance().allEditors
+//                for (editor in editors) {
+//                    if (editor !is EditorEx || editor.project != project) continue
+//
+//                    val file = editor.virtualFile
+//                    if (file == null || !file.isValid) continue
 
 //                    val schemaFiles = service.getSchemasForFile(file, false, true)
 //                    if (schemaFiles.contains { finalScope::contains }) {
@@ -84,7 +79,7 @@ class MiseTomlFileVfsListener private constructor(
 //                            .nonBlocking<Unit>(Callable { restartAnalyzer(analyzer, psiManager, file) })
 //                            .expireWith(service)
 //                            .submit(taskExecutor)
-                }
+//                }
             }
 
         fun onFileChange(file: VirtualFile) {
