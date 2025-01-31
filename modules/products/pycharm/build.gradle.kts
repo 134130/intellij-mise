ef1531d68b1c75a507be5e8cf8b6526c3548688e
JiHwan Oh <hwan_@kakao.com>
2025-02-01 00:27:01 +0900

    Support run configuration for PyCharm Community & Professional (#19)
    Closes: #10

---
+++ modules/products/pycharm/build.gradle.kts
@@ -0,0 +1,24 @@
+import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+
+fun properties(key: String) = project.findProperty(key).toString()
+
+plugins {
+    id("org.jetbrains.intellij.platform.module")
+    alias(libs.plugins.kotlin) // Kotlin support
+}
+
+dependencies {
+    implementation(project(":mise-core"))
+    testImplementation(libs.junit)
+
+    // Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
+    intellijPlatform {
+        create(IntelliJPlatformType.PyCharmCommunity, properties("platformVersion"))
+
+        bundledPlugins("PythonCore")
+
+        jetbrainsRuntime()
+        testFramework(TestFrameworkType.Platform)
+    }
+}
