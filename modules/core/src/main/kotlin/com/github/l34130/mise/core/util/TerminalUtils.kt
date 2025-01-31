02ea857705075431f1e8627a5745906594851a10
JiHwan Oh <hwan_@kakao.com>
2025-01-31 17:03:38 +0900

    deprecated method: TerminalToolWindowManager.createLocalShellWidget

--- modules/core/src/main/kotlin/com/github/l34130/mise/core/util/TerminalUtils.kt
+++ modules/core/src/main/kotlin/com/github/l34130/mise/core/util/TerminalUtils.kt
@@ -1,8 +1,10 @@
 package com.github.l34130.mise.core.util
 
+import com.github.l34130.mise.core.baseDirectory
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindowManager
+import org.jetbrains.plugins.terminal.ShellTerminalWidget
 import org.jetbrains.plugins.terminal.TerminalToolWindowFactory
 import org.jetbrains.plugins.terminal.TerminalToolWindowManager
 
@@ -12,8 +14,8 @@ object TerminalUtils {
         command: String,
         tabName: String? = null,
     ) {
-        val widget =
-            project.service<TerminalToolWindowManager>().createLocalShellWidget(project.basePath, tabName ?: "Mise")
+        val shellWidget = project.service<TerminalToolWindowManager>().createShellWidget(project.baseDirectory(), tabName ?: "Mise", true, true)
+        val widget = ShellTerminalWidget.toShellJediTermWidgetOrThrow(shellWidget)
 
         project
             .service<ToolWindowManager>()
