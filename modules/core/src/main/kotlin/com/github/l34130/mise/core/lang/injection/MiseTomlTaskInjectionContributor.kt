44d736e95841a65c9df2e5c6ac6a71a4351a24c1
JiHwan Oh <hwan_@kakao.com>
2025-02-01 16:51:10 +0900

    Support sh language injection

---
+++ modules/core/src/main/kotlin/com/github/l34130/mise/core/lang/injection/MiseTomlTaskInjectionContributor.kt
@@ -0,0 +1,15 @@
+package com.github.l34130.mise.core.lang.injection
+
+import com.github.l34130.mise.core.lang.psi.MiseTomlPsiPatterns
+import com.intellij.lang.injection.general.Injection
+import com.intellij.lang.injection.general.LanguageInjectionContributor
+import com.intellij.lang.injection.general.SimpleInjection
+import com.intellij.psi.PsiElement
+import com.intellij.sh.ShLanguage
+
+class MiseTomlTaskInjectionContributor : LanguageInjectionContributor {
+    override fun getInjection(context: PsiElement): Injection? {
+        if (!MiseTomlPsiPatterns.inTaskRunString.accepts(context)) return null
+        return SimpleInjection(ShLanguage.INSTANCE, "", "", null)
+    }
+}
