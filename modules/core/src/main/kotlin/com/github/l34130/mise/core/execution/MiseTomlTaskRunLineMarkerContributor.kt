e8f9251d25372635378bcdd905a463a03a6e7d59
JiHwan Oh <hwan_@kakao.com>
2025-02-08 20:33:30 +0900

    fix: RunLineMarkerContributor is contributing on non-leaf nodes (#167)
    ## Description
    
    - Fix `MiseTomlTaskRunLineMarkerContributor` is contribution on non-leaf
    nodes
    - It may occurs performance issues and Marker duplication.

--- modules/core/src/main/kotlin/com/github/l34130/mise/core/execution/MiseTomlTaskRunLineMarkerContributor.kt
+++ modules/core/src/main/kotlin/com/github/l34130/mise/core/execution/MiseTomlTaskRunLineMarkerContributor.kt
@@ -3,10 +3,12 @@ package com.github.l34130.mise.core.execution
 import com.github.l34130.mise.core.model.MiseTask
 import com.intellij.execution.lineMarker.RunLineMarkerContributor
 import com.intellij.psi.PsiElement
+import com.intellij.psi.impl.source.tree.LeafPsiElement
 import org.toml.lang.psi.TomlTable
 
 class MiseTomlTaskRunLineMarkerContributor : RunLineMarkerContributor() {
     override fun getInfo(element: PsiElement): Info? {
+        if (element !is LeafPsiElement) return null
         MiseTask.TomlTable.resolveOrNull(element)?.let { task ->
             return Info(RunMiseTomlTaskAction(task))
         }
