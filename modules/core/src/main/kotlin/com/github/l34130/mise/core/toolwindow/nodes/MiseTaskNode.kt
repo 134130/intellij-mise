f0b80bfd80ca70c2b4b5276a6855eb48aa99d6ec
JiHwan Oh <hwan_@kakao.com>
2025-02-09 19:06:42 +0900

    add support for right-menu

--- modules/core/src/main/kotlin/com/github/l34130/mise/core/toolwindow/nodes/MiseTaskNode.kt
+++ modules/core/src/main/kotlin/com/github/l34130/mise/core/toolwindow/nodes/MiseTaskNode.kt
@@ -2,14 +2,18 @@ package com.github.l34130.mise.core.toolwindow.nodes
 
 import com.github.l34130.mise.core.execution.RunMiseTomlTaskAction
 import com.github.l34130.mise.core.model.MiseTask
+import com.github.l34130.mise.core.toolwindow.ActionOnRightClick
 import com.github.l34130.mise.core.toolwindow.DoubleClickable
 import com.intellij.icons.AllIcons
 import com.intellij.ide.DataManager
 import com.intellij.ide.projectView.PresentationData
 import com.intellij.openapi.actionSystem.ActionPlaces
+import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.readAction
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.findPsiFile
+import com.intellij.util.OpenSourceUtil
 import kotlinx.coroutines.runBlocking
 import java.awt.event.MouseEvent
 
@@ -32,7 +36,8 @@ class MiseTaskNode(
         taskInfo,
         AllIcons.Debugger.Console,
     ),
-    DoubleClickable {
+    DoubleClickable,
+    ActionOnRightClick {
     override fun displayName(): String = taskInfo.name
 
     override fun createPresentation(): PresentationData =
@@ -56,4 +61,26 @@ class MiseTaskNode(
 
         action.actionPerformed(actionEvent)
     }
+
+    override fun actions(): List<AnAction> =
+        listOfNotNull(
+            RunMiseTomlTaskAction(taskInfo),
+            when (taskInfo) {
+                is MiseTask.ShellScript -> {
+                    object : AnAction("Go to Declaration", "Go to Declaration", AllIcons.General.Locate) {
+                        override fun actionPerformed(e: AnActionEvent) {
+                            OpenSourceUtil.navigateToSource(false, true, taskInfo.file.findPsiFile(project))
+                        }
+                    }
+                }
+                is MiseTask.TomlTable -> {
+                    object : AnAction("Go to Declaration", "Go to Declaration", AllIcons.General.Locate) {
+                        override fun actionPerformed(e: AnActionEvent) {
+                            OpenSourceUtil.navigateToSource(false, true, taskInfo.keySegment)
+                        }
+                    }
+                }
+                is MiseTask.Unknown -> null
+            },
+        )
 }
