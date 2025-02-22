fc019cc61c9e7560845a7bbf21d922bf469e28ab
JiHwan Oh <hwan_@kakao.com>
2025-02-22 17:26:11 +0900

    refactor: Remove unused originalContainingFile extension property from PsiElement

--- modules/core/src/main/kotlin/com/github/l34130/mise/core/model/MiseTask.kt
+++ modules/core/src/main/kotlin/com/github/l34130/mise/core/model/MiseTask.kt
@@ -7,11 +7,9 @@ import com.github.l34130.mise.core.lang.psi.or
 import com.github.l34130.mise.core.lang.psi.stringArray
 import com.github.l34130.mise.core.lang.psi.stringValue
 import com.intellij.openapi.actionSystem.DataKey
-import com.intellij.openapi.util.UserDataHolder
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
-import com.intellij.testFramework.LightVirtualFile
 import org.toml.lang.psi.TomlInlineTable
 import org.toml.lang.psi.TomlKey
 import org.toml.lang.psi.TomlKeySegment
@@ -34,13 +32,6 @@ sealed interface MiseTask {
     companion object {
         val DATA_KEY = DataKey.create<MiseTask>(javaClass.simpleName)
     }
-
-    private val PsiElement.originalContainingFile: UserDataHolder?
-        get() {
-            val containingFile = this.containingFile
-            return (containingFile.viewProvider.virtualFile as? LightVirtualFile)?.originalFile
-                ?: containingFile
-        }
 }
 
 class MiseUnknownTask internal constructor(
