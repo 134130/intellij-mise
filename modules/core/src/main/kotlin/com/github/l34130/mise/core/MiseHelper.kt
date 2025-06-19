03e86c6174c46f6147750eb156943f0c718bc576
JiHwan Oh <hwan_@kakao.com>
2025-06-19 20:50:23 +0900

    fix: Calling invokeAndWait from read-action leads to possible deadlock (#272)
    - `SpringBootRunConfiguration` is calls the
    `RunConfigurationExtension.updateJavaParameters` on EDT with
    read-action.
    - So opening the progress modal makes deadlock.
    - Check the current thread's state and do the best option on the case.
    (The worst, synchronized loading)
    
    Resolves: #259
    
    - The SpringBootRunConfiguration calls the
    `RunConfigurationExtension.updateJavaParameters` on UI Thread.
    -
    https://youtrack.jetbrains.com/issue/IJPL-14185/Threading-issues-with-RunConfigurationExtension

--- modules/core/src/main/kotlin/com/github/l34130/mise/core/MiseHelper.kt
+++ modules/core/src/main/kotlin/com/github/l34130/mise/core/MiseHelper.kt
@@ -7,8 +7,11 @@ import com.github.l34130.mise.core.run.MiseRunConfigurationSettingsEditor
 import com.github.l34130.mise.core.setting.MiseProjectSettings
 import com.intellij.execution.configurations.RunConfigurationBase
 import com.intellij.openapi.components.service
-import com.intellij.openapi.progress.ProgressManager
-import com.intellij.openapi.util.ThrowableComputable
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.diagnostic.debug
+import com.intellij.openapi.progress.ProcessCanceledException
+import com.intellij.platform.ide.progress.runWithModalProgressBlocking
+import com.intellij.util.application
 import java.util.function.Supplier
 
 object MiseHelper {
@@ -31,23 +34,38 @@ object MiseHelper {
                 else -> return emptyMap()
             }
 
-        return ProgressManager.getInstance().runProcessWithProgressSynchronously(
-            ThrowableComputable {
-                MiseCommandLineHelper
-                    .getEnvVars(workDir, configEnvironment)
-                    .fold(
-                        onSuccess = { envVars -> envVars },
-                        onFailure = {
-                            if (it !is MiseCommandLineNotFoundException) {
-                                MiseNotificationServiceUtils.notifyException("Failed to load environment variables", it, project)
-                            }
-                            mapOf()
-                        },
-                    )
-            },
-            "Loading Mise Environment Variables",
-            true,
-            configuration.project,
-        )
+        val result =
+            if (application.isDispatchThread) {
+                logger.debug { "dispatch thread detected, loading env vars on current thread" }
+                runWithModalProgressBlocking(project, "Loading Mise Environment Variables") {
+                    MiseCommandLineHelper.getEnvVars(workDir, configEnvironment)
+                }
+            } else if (!application.isReadAccessAllowed) {
+                logger.debug { "no read lock detected, loading env vars on dispatch thread" }
+                var result: Result<Map<String, String>>? = null
+                application.invokeAndWait {
+                    logger.debug { "loading env vars on invokeAndWait" }
+                    runWithModalProgressBlocking(project, "Loading Mise Environment Variables") {
+                        result = MiseCommandLineHelper.getEnvVars(workDir, configEnvironment)
+                    }
+                }
+                result ?: throw ProcessCanceledException()
+            } else {
+                logger.debug { "unable to open the dialog. just load synchronously" }
+                MiseCommandLineHelper.getEnvVars(workDir, configEnvironment)
+            }
+
+        return result
+            .fold(
+                onSuccess = { envVars -> envVars },
+                onFailure = {
+                    if (it !is MiseCommandLineNotFoundException) {
+                        MiseNotificationServiceUtils.notifyException("Failed to load environment variables", it, project)
+                    }
+                    mapOf()
+                },
+            )
     }
+
+    private val logger = Logger.getInstance(MiseHelper::class.java)
 }
