d7c6d6e460361e61d4040cd99100375b822a73cf
JiHwan Oh <sapindus.hwan@gmail.com>
2025-09-25 17:02:22 +0900

    refactor: Introduce MiseConfigFileResolver (#332)

--- modules/core/src/main/kotlin/com/github/l34130/mise/core/MiseProjectService.kt
+++ modules/core/src/main/kotlin/com/github/l34130/mise/core/MiseProjectService.kt
@@ -1,31 +1,21 @@
 package com.github.l34130.mise.core
 
-import com.github.l34130.mise.core.model.MiseShellScriptTask
 import com.github.l34130.mise.core.model.MiseTask
-import com.github.l34130.mise.core.model.MiseTomlFile
-import com.github.l34130.mise.core.model.MiseTomlTableTask
-import com.github.l34130.mise.core.util.baseDirectory
+import com.intellij.ide.impl.ProjectUtil
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.readAction
 import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.VirtualFileManager
-import com.intellij.openapi.vfs.findPsiFile
-import com.intellij.openapi.vfs.resolveFromRootOrRelative
-import com.intellij.openapi.vfs.toNioPathOrNull
-import com.intellij.psi.util.childrenOfType
 import com.intellij.util.application
-import com.intellij.util.containers.addIfNotNull
 import com.jetbrains.rd.util.ConcurrentHashMap
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import org.toml.lang.psi.TomlFile
-import org.toml.lang.psi.TomlTable
 import java.util.concurrent.atomic.AtomicBoolean
-import kotlin.io.path.isExecutable
 
 @Service(Service.Level.PROJECT)
 class MiseProjectService(
@@ -35,8 +25,6 @@ class MiseProjectService(
     var isInitialized: AtomicBoolean = AtomicBoolean(false)
         private set
 
-    private val miseTomlFiles: MutableSet<VirtualFile> = ConcurrentHashMap.newKeySet<VirtualFile>()
-    private val taskConfigDirectories: MutableSet<VirtualFile> = ConcurrentHashMap.newKeySet<VirtualFile>()
     private val tasks: MutableSet<MiseTask> = ConcurrentHashMap.newKeySet<MiseTask>()
 
     init {
@@ -53,21 +41,11 @@ class MiseProjectService(
         }
     }
 
-    fun getMiseTomlFiles(): Set<VirtualFile> = miseTomlFiles
-
-    fun getFileTaskDirectories(): Set<VirtualFile> = taskConfigDirectories
-
     fun getTasks(): Set<MiseTask> = tasks
 
     suspend fun refresh() {
-        miseTomlFiles.clear()
-        taskConfigDirectories.clear()
         tasks.clear()
 
-        // TODO: Use [MiseTaskResolver]
-
-        loadMiseTomlFiles()
-        loadFileTaskDirectories()
         loadTasks()
 
         isInitialized.set(true)
@@ -76,146 +54,17 @@ class MiseProjectService(
     override fun dispose() {
     }
 
-    private suspend fun loadMiseTomlFiles() {
-        readAction {
-            val result = mutableListOf<VirtualFile>()
-
-            val baseDir =
-                if (application.isUnitTestMode) {
-                    VirtualFileManager.getInstance().findFileByUrl("temp:///src") ?: return@readAction
-                } else {
-                    LocalFileSystem.getInstance().findFileByPath(project.baseDirectory()) ?: return@readAction
-                }
-
-            for (child in baseDir.children) {
-                if (child.isDirectory) {
-                    // mise/config.toml or .mise/config.toml
-                    if (child.name == "mise" || child.name == ".mise") {
-                        result.addIfNotNull(child.resolveFromRootOrRelative("config.toml"))
-                    }
-
-                    if (child.name == ".config") {
-                        // .config/mise.toml
-                        result.addIfNotNull(child.resolveFromRootOrRelative("mise.toml"))
-                        // .config/mise/config.toml
-                        result.addIfNotNull(child.resolveFromRootOrRelative("config.toml"))
-                        // .config/mise/conf.d/*.toml
-                        result.addAll(child.resolveFromRootOrRelative("conf.d")?.children.orEmpty())
-                    }
-                } else {
-                    // mise.local.toml, mise.toml, .mise.local.toml, .mise.toml
-                    if (child.name.matches(MISE_TOML_NAME_REGEX)) {
-                        result.add(child)
-                    }
-                }
-            }
-
-            miseTomlFiles.addAll(result)
-        }
-    }
-
-    private suspend fun loadFileTaskDirectories() {
-        readAction {
-            val result = mutableListOf<VirtualFile>()
-
-            val baseDir =
-                if (application.isUnitTestMode) {
-                    VirtualFileManager.getInstance().findFileByUrl("temp:///src") ?: return@readAction
-                } else {
-                    LocalFileSystem.getInstance().findFileByPath(project.baseDirectory()) ?: return@readAction
-                }
-
-            result.addIfNotNull(baseDir.resolveFromRootOrRelative("mise-tasks")?.takeIf { it.isDirectory })
-            result.addIfNotNull(baseDir.resolveFromRootOrRelative(".mise-tasks")?.takeIf { it.isDirectory })
-            result.addIfNotNull(baseDir.resolveFromRootOrRelative("mise/tasks")?.takeIf { it.isDirectory })
-            result.addIfNotNull(baseDir.resolveFromRootOrRelative(".mise/tasks")?.takeIf { it.isDirectory })
-            result.addIfNotNull(baseDir.resolveFromRootOrRelative(".config/mise/tasks")?.takeIf { it.isDirectory })
-
-            for (miseToml in getMiseTomlFiles()) {
-                val psiFile = miseToml.findPsiFile(project) as? TomlFile ?: continue
-
-                val taskConfig = MiseTomlFile.TaskConfig.resolveOrNull(psiFile) ?: continue
-                val taskTomlOrDirs = taskConfig.includes ?: continue
-
-                for (taskTomlOrDir in taskTomlOrDirs) {
-                    val file = baseDir.resolveFromRootOrRelative(taskTomlOrDir) ?: continue
-                    if (file.isDirectory) {
-                        result.addIfNotNull(file)
-                    }
-                }
-            }
-
-            taskConfigDirectories.addAll(result)
-        }
-    }
-
     private suspend fun loadTasks() {
-        readAction {
-            val result = mutableListOf<MiseTask>()
-
-            val baseDir =
+        val baseDir: VirtualFile =
+            readAction {
                 if (application.isUnitTestMode) {
-                    VirtualFileManager.getInstance().findFileByUrl("temp:///src") ?: return@readAction
+                    VirtualFileManager.getInstance().findFileByUrl("temp:///src")
                 } else {
-                    LocalFileSystem.getInstance().findFileByPath(project.baseDirectory()) ?: return@readAction
+                    LocalFileSystem.getInstance().findFileByPath(project.basePath ?: ProjectUtil.getBaseDir())
                 }
+            } ?: return
 
-            // get all tasks from all toml files in the project
-            for (virtualFile in getMiseTomlFiles()) {
-                val psiFile = virtualFile.findPsiFile(project) as? TomlFile ?: continue
-                result.addAll(MiseTomlTableTask.resolveAllFromTomlFile(psiFile))
-            }
-
-            // get all tasks from the task directories
-            for (directory in getFileTaskDirectories()) {
-                directory
-                    .leafChildren()
-                    .filter {
-                        // if it's not a real file, consider it as executable
-                        val nioPath = it.toNioPathOrNull() ?: return@filter true
-                        nioPath.isExecutable()
-                    }.mapNotNullTo(result) {
-                        MiseShellScriptTask.resolveOrNull(directory, it)
-                    }
-            }
-
-            // get all tasks from the task config includes
-            for (miseToml in getMiseTomlFiles()) {
-                val psiFile = miseToml.findPsiFile(project) as? TomlFile ?: continue
-
-                val taskConfig = MiseTomlFile.TaskConfig.resolveOrNull(psiFile) ?: continue
-                val taskTomlOrDirs = taskConfig.includes ?: continue
-
-                for (taskTomlOrDir in taskTomlOrDirs) {
-                    val virtualFile = baseDir.resolveFromRootOrRelative(taskTomlOrDir) ?: continue
-                    if (!virtualFile.isDirectory) {
-                        val psiFile = virtualFile.findPsiFile(project) as? TomlFile ?: continue
-                        val tables = psiFile.childrenOfType<TomlTable>()
-                        for (table in tables) {
-                            val keySegments = table.header.key?.segments ?: continue
-                            val keySegment = keySegments.singleOrNull() ?: continue
-                            result.addIfNotNull(MiseTomlTableTask.resolveOrNull(keySegment))
-                        }
-                    }
-                }
-            }
-
-            tasks.addAll(result)
-        }
-    }
-
-    private fun VirtualFile.leafChildren(): Sequence<VirtualFile> =
-        children.asSequence().flatMap {
-            if (it.isDirectory) {
-                it.leafChildren()
-            } else {
-                sequenceOf(it)
-            }
-        }
-
-    companion object {
-        private val MISE_TOML_NAME_REGEX = "^\\.?mise\\.(\\w+\\.)?toml$".toRegex()
-
-        fun getInstance(project: Project): MiseProjectService = project.getService(MiseProjectService::class.java)
+        val miseTasks = project.service<MiseTaskResolver>().getMiseTasks(baseDir, true)
+        tasks.addAll(miseTasks)
     }
 }
