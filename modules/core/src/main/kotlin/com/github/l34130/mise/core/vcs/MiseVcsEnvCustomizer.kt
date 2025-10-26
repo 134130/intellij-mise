637e1bbee9f882b2d8a806055e99d4d357c07c85
JiHwan Oh <sapindus.hwan@gmail.com>
2025-10-26 14:12:18 +0900

    handle InterruptedException gracefully

--- modules/core/src/main/kotlin/com/github/l34130/mise/core/vcs/MiseVcsEnvCustomizer.kt
+++ modules/core/src/main/kotlin/com/github/l34130/mise/core/vcs/MiseVcsEnvCustomizer.kt
@@ -8,9 +8,12 @@ import com.github.l34130.mise.core.setting.MiseProjectSettings
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.options.UnnamedConfigurable
+import com.intellij.openapi.progress.ProcessCanceledException
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.VcsEnvCustomizer
 import com.intellij.platform.ide.progress.withBackgroundProgress
+import com.intellij.util.progress.sleepCancellable
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.runBlocking
 
 class MiseVcsEnvCustomizer : VcsEnvCustomizer() {
@@ -26,28 +29,39 @@ class MiseVcsEnvCustomizer : VcsEnvCustomizer() {
         val miseProjectSettings = project.service<MiseProjectSettings>().state
         if (!miseProjectSettings.useMiseDirEnv) return
 
-        val miseEnvsResult =
-            runBlocking {
-                withBackgroundProgress(project, "Mise: Getting EnvVars") {
-                    MiseCommandLineHelper.getEnvVars(project.basePath, miseProjectSettings.miseConfigEnvironment)
+        try {
+            // TODO: Cache the env vars to avoid running the command every time
+            val miseEnvsResult =
+                runBlocking {
+                    withBackgroundProgress(project, "Mise: Getting EnvVars") {
+                        MiseCommandLineHelper.getEnvVars(project.basePath, miseProjectSettings.miseConfigEnvironment)
+                    }
                 }
-            }
 
-        miseEnvsResult.fold(
-            onSuccess = { envs.putAll(it) },
-            onFailure = {
-                logger.warn("Failed to get Mise env vars", it)
-                MiseNotificationService.getInstance(project).warn(
-                    title = "Failed to get Mise env vars",
-                    htmlText =
-                        """
-                        Failed to get Mise env vars for VCS operations.<br/>
-                        ${it.message}<br/>
-                        Please check your Mise configuration.<br/>
-                        """.trimIndent(),
-                )
-            },
-        )
+            miseEnvsResult.fold(
+                onSuccess = { envs.putAll(it) },
+                onFailure = {
+                    logger.warn("Failed to get Mise env vars", it)
+                    MiseNotificationService.getInstance(project).warn(
+                        title = "Failed to get Mise env vars",
+                        htmlText =
+                            """
+                            Failed to get Mise env vars for VCS operations.<br/>
+                            ${it.message}<br/>
+                            Please check your Mise configuration.<br/>
+                            """.trimIndent(),
+                    )
+                },
+            )
+        } catch (e: CancellationException) {
+            if (e is ProcessCanceledException) {
+                throw e
+            }
+            throw ProcessCanceledException(e)
+        } catch (e: InterruptedException) {
+            Thread.currentThread().interrupt()
+            throw ProcessCanceledException(e)
+        }
     }
 
     override fun getConfigurable(project: Project?): UnnamedConfigurable? {
