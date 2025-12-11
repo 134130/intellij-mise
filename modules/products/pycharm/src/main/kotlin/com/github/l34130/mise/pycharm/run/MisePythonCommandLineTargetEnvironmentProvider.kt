7c3090824ed42b6e0505a2a68106bc4900fce92d
Tomas Drencak <tomas@drencak.com>
2025-12-11 10:14:22 +0100

    feat(pycharm): support mise env vars in Python Console
    Extend PythonCommandLineTargetEnvironmentProvider to inject mise
    environment variables for Python Console and Flask Console, not
    just run configurations.

--- modules/products/pycharm/src/main/kotlin/com/github/l34130/mise/pycharm/run/MisePythonCommandLineTargetEnvironmentProvider.kt
+++ modules/products/pycharm/src/main/kotlin/com/github/l34130/mise/pycharm/run/MisePythonCommandLineTargetEnvironmentProvider.kt
@@ -1,8 +1,9 @@
 package com.github.l34130.mise.pycharm.run
 
 import com.github.l34130.mise.core.MiseHelper
+import com.github.l34130.mise.core.setting.MiseProjectSettings
+import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
-import com.jetbrains.python.run.AbstractPythonRunConfiguration
 import com.jetbrains.python.run.PythonExecution
 import com.jetbrains.python.run.PythonRunParams
 import com.jetbrains.python.run.target.HelpersAwareTargetEnvironmentRequest
@@ -16,16 +17,17 @@ class MisePythonCommandLineTargetEnvironmentProvider : PythonCommandLineTargetEn
         pythonExecution: PythonExecution,
         runParams: PythonRunParams,
     ) {
-        if (runParams is AbstractPythonRunConfiguration<*>) {
-            val envs =
-                MiseHelper.getMiseEnvVarsOrNotify(
-                    configuration = runParams,
-                    workingDirectory = { runParams.workingDirectory },
-                )
+        val projectState = project.service<MiseProjectSettings>().state
+        if (!projectState.useMiseDirEnv) return
 
-            for ((key, value) in runParams.envs + envs) {
-                pythonExecution.addEnvironmentVariable(key, value)
-            }
+        val envs = MiseHelper.getMiseEnvVarsOrNotify(
+            project = project,
+            workDir = project.basePath,
+            configEnvironment = projectState.miseConfigEnvironment,
+        )
+
+        for ((key, value) in runParams.envs + envs) {
+            pythonExecution.addEnvironmentVariable(key, value)
         }
     }
 }
