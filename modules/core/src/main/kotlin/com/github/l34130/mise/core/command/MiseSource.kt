844eb421e0ae77fb1e966944bfbb726ec368cc9c
JiHwan Oh <sapindus.hwan@gmail.com>
2025-12-14 14:06:26 +0900

    fix: update JsonProperty annotations to use @field syntax

--- modules/core/src/main/kotlin/com/github/l34130/mise/core/command/MiseSource.kt
+++ modules/core/src/main/kotlin/com/github/l34130/mise/core/command/MiseSource.kt
@@ -3,8 +3,8 @@ package com.github.l34130.mise.core.command
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class MiseSource(
-    @JsonProperty("type")
+    @field:JsonProperty("type")
     val fileName: String, // .mise.toml, .mise.config, etc.
-    @JsonProperty("path")
+    @field:JsonProperty("path")
     val absolutePath: String, // absolute path to the source file
 )
