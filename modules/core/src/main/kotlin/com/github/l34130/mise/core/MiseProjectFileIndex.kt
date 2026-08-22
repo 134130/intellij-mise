e13ed85cfb58c8f7ba2723893efaca2e92f7dbcb
JiHwan Oh <sapindus.hwan@gmail.com>
2026-08-22 17:56:35 +0900

    Fix read action for project file index access (#522)

--- modules/core/src/main/kotlin/com/github/l34130/mise/core/MiseProjectFileIndex.kt
+++ modules/core/src/main/kotlin/com/github/l34130/mise/core/MiseProjectFileIndex.kt
@@ -1,10 +1,17 @@
 package com.github.l34130.mise.core
 
+import com.intellij.openapi.application.runReadActionBlocking
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.vfs.VirtualFile
 
 internal fun Project.isExcludedFromMiseResolution(file: VirtualFile): Boolean {
     if (isDisposed || !file.isValid) return false
-    return ProjectFileIndex.getInstance(this).isExcluded(file)
+    return runReadActionBlocking {
+        if (isDisposed || !file.isValid) {
+            false
+        } else {
+            ProjectFileIndex.getInstance(this).isExcluded(file)
+        }
+    }
 }
